7866e2adbaf5ac811eb9349ccbf6fbe5a3db9090
embersight <justincpresley@gmail.com>
2020-08-27 23:05:22 -0500

    Indicate an Address on Client

--- ping_a_port.py
+++ ping_a_port.py
@@ -3,20 +3,24 @@ import socket
 
 def main():
     # Command Line Parser
-    parser = ArgumentParser(add_help=False,description="Ping a port on the current network")
+    parser = ArgumentParser(add_help=False,description="Ping a port on a certain network")
     requiredArgs = parser.add_argument_group("required arguments")
     optionalArgs = parser.add_argument_group("optional arguments")
     # Adding All Command Line Arguments
-    requiredArgs.add_argument("-p","--port",required=True,help="port to listen on")
+    requiredArgs.add_argument("-a","--address",required=True,help="address to ping")
+    requiredArgs.add_argument("-p","--port",required=True,help="port to ping")
     optionalArgs.add_argument("-h","--help",action="help",default=SUPPRESS,help="show this help message and exit")
     # Getting All Arugments
     args = vars(parser.parse_args())
 
     port = int(args["port"])
+    address = args["address"]
+    if not address:
+        address = socket.gethostname()
 
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((socket.gethostname(), port))
+        s.connect((address, port))
         msg = s.recv(1024)
         print("Message from Server: " + msg.decode("utf-8"))
     except socket.error as err:
