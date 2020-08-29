5485844a51a83f89b778701ff217b6121a24419f
embersight <justincpresley@gmail.com>
2020-08-29 15:44:01 -0500

    Logging on Both Ends

--- listen_to_port.py
+++ listen_to_port.py
@@ -1,5 +1,7 @@
 from argparse import ArgumentParser, SUPPRESS
 import socket
+import struct
+import logging
 
 def main():
     # Command Line Parser
@@ -7,34 +9,36 @@ def main():
     requiredArgs = parser.add_argument_group("required arguments")
     optionalArgs = parser.add_argument_group("optional arguments")
     # Adding All Command Line Arguments
-    requiredArgs.add_argument("-p","--port",required=True,help="port to listen on")
+    requiredArgs.add_argument("-p","--port",required=True,help="the port server listens on")
+    requiredArgs.add_argument("-l","--logfile",required=True,help="where it will keep a record of actions")
     optionalArgs.add_argument("-h","--help",action="help",default=SUPPRESS,help="show this help message and exit")
     # Getting All Arugments
     args = vars(parser.parse_args())
 
     port = int(args["port"])
+    logpath = str(args["logfile"])
+
+    logging.basicConfig(filename=logpath, filemode='w', format='%(asctime)s | %(levelname)s : %(message)s', datefmt='%d-%B-%Y %H:%M:%S')
 
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print(f'Sodcket successfully created')
-
+        logging.info(f'Socket successfully created.')
         s.bind((socket.gethostname(), port))
-        print(f'Socket binded to {port}')
-
+        logging.info(f'Socket binded to {port}.')
         s.listen(5)
-        print(f'Socket is listening')
+        logging.info(f'Socket is listening.')
 
         while True:
             clientsocket, address = s.accept()
-            print(f"Connection from {address} has been established.")
-            print(f"Sent a message to that address.")
+            logging.info(f'Connection from {address} has been established.')
+            logging.info(f'Sent a message to that address.')
             clientsocket.send(bytes("Hello!","utf-8"))
             clientsocket.close()
 
     except socket.error as err:
-        print(f'Socket creation failed with error {err}')
+        logging.error(f'Socket creation failed with error {err}.')
     except:
-        print(f'Unknown error has occured with Socket')
+        logging.error(f'Unknown error has occured with Socket.')
 
 
 if __name__ == '__main__':
--- ping_a_port.py
+++ ping_a_port.py
@@ -1,5 +1,7 @@
 from argparse import ArgumentParser, SUPPRESS
 import socket
+import struct
+import logging
 
 def main():
     # Command Line Parser
@@ -7,26 +9,30 @@ def main():
     requiredArgs = parser.add_argument_group("required arguments")
     optionalArgs = parser.add_argument_group("optional arguments")
     # Adding All Command Line Arguments
-    requiredArgs.add_argument("-a","--address",required=True,help="address to ping")
-    requiredArgs.add_argument("-p","--port",required=True,help="port to ping")
+    requiredArgs.add_argument("-s","--server",required=True,help="the IP address of server")
+    requiredArgs.add_argument("-p","--port",required=True,help="the port the server listens on")
+    requiredArgs.add_argument("-l","--logfile",required=True,help="where it will keep a record of actions")
     optionalArgs.add_argument("-h","--help",action="help",default=SUPPRESS,help="show this help message and exit")
     # Getting All Arugments
     args = vars(parser.parse_args())
 
     port = int(args["port"])
-    address = args["address"]
-    if not address:
-        address = socket.gethostname()
+    address = str(args["server"])
+    logpath = str(args["logfile"])
+
+    logging.basicConfig(filename=logpath, filemode='w', format='%(asctime)s | %(levelname)s : %(message)s', datefmt='%d-%B-%Y %H:%M:%S')
 
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        logging.info(f'Socket successfully created.')
         s.connect((address, port))
+        logging.info(f'Socket connected to {address}, {port}.')
         msg = s.recv(1024)
-        print("Message from Server: " + msg.decode("utf-8"))
+        logging.info(f'Message from server: {msg.decode("utf-8")}.')
     except socket.error as err:
-        print(f'Socket creation failed with error {err}')
+        logging.error(f'Socket creation failed with error {err}.')
     except:
-        print(f'Unknown error has occured with Socket')
+        logging.error(f'Unknown error has occured with socket.')
 
 
 if __name__ == '__main__':
